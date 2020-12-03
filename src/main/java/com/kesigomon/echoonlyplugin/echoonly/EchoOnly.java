e3fe9ee46141a02f71aecf3cdc155166ebab6131
Kesigomon <kesigomon@gmail.com>
2020-12-03 14:44:02 +0900

    first commit

---
+++ src/main/java/com/kesigomon/echoonlyplugin/echoonly/EchoOnly.java
@@ -0,0 +1,17 @@
+package com.kesigomon.echoonlyplugin.echoonly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EchoOnly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
